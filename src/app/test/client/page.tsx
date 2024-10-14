71253f6cd4752cf9b94b57ab56a8309782b8213d
Rudra Patel <patelrudra2003@gmail.com>
2024-10-14 13:42:39 -0400

    feat: Move emails and protect dashboard subroutes

--- src/app/test/client/page.tsx
+++ src/app/test/client/page.tsx
@@ -1,5 +1,22 @@
 "use client";
 
+import { signOut } from "next-auth/react";
+
 export default function ClientComponentTestPage() {
-  return <p>Client component test page</p>;
+  const handleSignOut = () => {
+    signOut()
+      .then(() => {
+        alert("Signed out");
+      })
+      .catch(() => {
+        alert("Error signing out");
+      });
+  };
+
+  return (
+    <>
+      <p>Client component test page</p>
+      <button onClick={handleSignOut}>Sign out</button>
+    </>
+  );
 }
