85607b2d0f11e1a85a8e534040d23a2f97b0dd8b
Alan Khalili <khalilialan@gmail.com>
2024-10-18 21:54:46 -0400

    Fix: reverted page.tsx back to OG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import React from "react";
-import WelcomeEmail from "@/components/emails/WelcomeEmail";
-
 export default function Home() {
-  return (
-    <>
-      <WelcomeEmail firstName="John" programName="Test Program" />
-    </>
-  );
+  return <p>Home page</p>;
 }
