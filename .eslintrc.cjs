ac77804ae8b5caae8a7c451f2459a33f5800eca5
Rudra Patel <patelrudra2003@gmail.com>
2024-10-20 12:32:31 -0400

    fix: Fix eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,7 @@ module.exports = {
   root: true,
   rules: {
     "prettier/prettier": "warn",
-    "@typescript-eslint/consistent-type-definitions": ["warn", "error"],
+    "@typescript-eslint/consistent-type-definitions": ["error", "tye"],
     "no-console": "warn",
     "no-restricted-imports": [
       "error",
