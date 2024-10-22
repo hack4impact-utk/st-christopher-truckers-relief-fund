4876ba26c6594a85b31d3fbd78e5abefc9656c1e
Alan Khalili <khalilialan@gmail.com>
2024-10-22 10:48:57 -0400

    WIP email should be sending but getting a console message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
+import { render } from "@react-email/components";
+import nodemailer from "nodemailer";
+
+import WelcomeEmail from "@/components/emails/WelcomeEmail";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.forwardemail.net",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.SCF_GMAIL,
+    pass: process.env.SCF_GMAIL_APP_PASSWORD,
+  },
+});
+
+async function sendEmail() {
+  const emailHtml = await render(
+    <WelcomeEmail firstName="Alan" programName="Test" />,
+  );
+
+  const options = {
+    from: process.env.SCF_GMAIL,
+    to: "khalilialan@gmail.com", // Replace with your email
+    subject: "hello world",
+    html: emailHtml,
+  };
+
+  await transporter.sendMail(options);
+  console.log("Email sent");
+}
+
+sendEmail();
+
 export default function Home() {
   return <p>Home page</p>;
 }
