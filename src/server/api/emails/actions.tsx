e6fa893168cdbde56919395a9f1ed94954af0243
Kelly Luong <kellyluong06@gmail.com>
2024-10-23 20:48:13 -0500

    feat: Made PasswordChanged Component (#26)

--- src/server/api/emails/actions.tsx
+++ src/server/api/emails/actions.tsx
@@ -2,12 +2,16 @@
 
 import { render } from "@react-email/components";
 
+import PasswordChanged from "@/components/emails/PasswordChanged";
 import ResetPasswordEmail from "@/components/emails/ResetPasswordEmail";
 import WelcomeEmail from "@/components/emails/WelcomeEmail";
 import sendEmail from "@/server/api/emails/helpers";
 
-export async function sendPasswordChangeEmail(recipient_email: string) {
-  const html = `<p>Hello ${recipient_email}</p>`;
+export async function sendPasswordChangeEmail(
+  recipient_email: string,
+  firstName: string,
+) {
+  const html = await render(<PasswordChanged firstName={firstName} />);
 
   await sendEmail(recipient_email, "Your SCF password has been changed", html);
 }
