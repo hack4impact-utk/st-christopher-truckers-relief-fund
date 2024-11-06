dbf8cba78e198aa8f1324185a2684cc3f7dbcf21
Trishu Patel <141292044+Trishu-Patel@users.noreply.github.com>
2024-11-06 14:17:07 -0500

    feat: Create change password server action (#45)
    Co-authored-by: Rudra Patel <patelrudra2003@gmail.com>

--- src/server/api/users/mutations.ts
+++ src/server/api/users/mutations.ts
@@ -8,6 +8,7 @@ import { updateUser } from "@/server/api/users/secure-mutations";
 import dbConnect from "@/server/dbConnect";
 import { UserModel } from "@/server/models";
 import { ApiResponse, User } from "@/types";
+import authenticateServerFunction from "@/utils/authenticateServerFunction";
 import apiErrors from "@/utils/constants/apiErrors";
 import handleMongooseError from "@/utils/handleMongooseError";
 
@@ -55,6 +56,50 @@ export async function resetPasswordWithToken(
 
   if (userError !== null) {
     return [null, userError];
+  }
+
+  const hashedPassword = await bcrypt.hash(newPassword, 10);
+  user.password = hashedPassword;
+
+  await updateUser(user);
+
+  return [null, null];
+}
+
+export async function changePassword(
+  email: string,
+  oldPassword: string,
+  newPassword: string,
+): Promise<ApiResponse<null>> {
+  await dbConnect();
+
+  const [session, sessionError] = await authenticateServerFunction();
+
+  if (sessionError !== null) {
+    return [null, sessionError];
+  }
+
+  if (oldPassword.length < 8 || newPassword.length < 8) {
+    return [null, apiErrors.user.userInvalidCredentials];
+  }
+
+  if (session.user.email !== email) {
+    return [null, apiErrors.user.userInvalidCredentials];
+  }
+
+  const [user, userError] = await getUserByEmail(email);
+
+  if (userError !== null) {
+    return [null, userError];
+  }
+
+  const doesOldPasswordsMatch = await bcrypt.compare(
+    oldPassword,
+    user.password,
+  );
+
+  if (!doesOldPasswordsMatch) {
+    return [null, apiErrors.user.userInvalidCredentials];
   }
 
   const hashedPassword = await bcrypt.hash(newPassword, 10);
