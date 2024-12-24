9fb6780bac82fa3451c33f898d1b013722087fb4
Rudra Patel <patelrudra2003@gmail.com>
2024-12-24 15:54:44 -0600

    feat: Add global loading indicator

---
+++ src/app/loading.tsx
@@ -0,0 +1,16 @@
+import { Box, CircularProgress } from "@mui/material";
+
+export default function GlobalLoadingSkeleton() {
+  return (
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <CircularProgress />
+    </Box>
+  );
+}
