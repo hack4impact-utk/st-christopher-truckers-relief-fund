4bb75b636c0afca3b51e953ef5739dac3219fcb9
Kelly Luong <kellyluong06@gmail.com>
2025-02-09 17:48:01 -0500

    feat: Added Rigs Without Cigs Prize Utility Function

---
+++ src/utils/prizeFunction.ts
@@ -0,0 +1,21 @@
+import dayjs, { Dayjs } from "dayjs";
+
+export type PrizeTimes = {
+  timeUntil1Month: number;
+  timeUntil6Months: number;
+  timeUntil1Year: number;
+};
+
+export default function timeUntilNextPrize(date: Dayjs): PrizeTimes {
+  const now = dayjs();
+
+  const oneMonth = date.add(1, "month");
+  const sixMonths = date.add(6, "month");
+  const oneYear = date.add(1, "year");
+
+  return {
+    timeUntil1Month: now.isAfter(oneMonth) ? 0 : oneMonth.diff(now, "day"),
+    timeUntil6Months: now.isAfter(sixMonths) ? 0 : sixMonths.diff(now, "day"),
+    timeUntil1Year: now.isAfter(oneYear) ? 0 : oneYear.diff(now, "day"),
+  };
+}
