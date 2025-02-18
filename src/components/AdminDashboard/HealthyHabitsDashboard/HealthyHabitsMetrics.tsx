0ec3d4ba08b91f2ae9b305c6b62d0c86d630598d
Shashank Bandaru <shashanksr.bandaru@gmail.com>
2025-02-18 16:12:26 -0500

    set up healthyhabits metrics ui

--- src/components/AdminDashboard/HealthyHabitsDashboard/HealthyHabitsMetrics.tsx
+++ src/components/AdminDashboard/HealthyHabitsDashboard/HealthyHabitsMetrics.tsx
@@ -3,10 +3,43 @@
 import { Box, Typography } from "@mui/material";
 import { ReactNode } from "react";
 
-export default function HealthyHabitsMetrics(): ReactNode {
+import { ProgramEnrollment } from "@/types";
+
+type HealthyHabitsMetricsProps = {
+  HealthyHabitsProgramEnrollments: ProgramEnrollment[];
+};
+
+export default function HealthyHabitsMetrics({
+  HealthyHabitsProgramEnrollments,
+}: HealthyHabitsMetricsProps): ReactNode {
+  const totalEnrolled = HealthyHabitsProgramEnrollments.length;
+  const totalActive = 50;
+  const registrationsInPast3Months = 20;
+
   return (
-    <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
-      <Typography variant="h6">Metrics</Typography>
+    <Box
+      sx={{
+        width: "min(90vw, 700px)",
+        boxShadow: 3,
+        borderRadius: 2,
+        padding: 4,
+      }}
+    >
+      <Typography variant="h5" textAlign="center" gutterBottom>
+        Metrics
+      </Typography>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+        <Typography>
+          <strong>Total Erolled:</strong> {totalEnrolled}
+        </Typography>
+        <Typography>
+          <strong>Total Active:</strong> {totalActive}
+        </Typography>
+        <Typography>
+          <strong>Registrations in Past 3 Months:</strong>{" "}
+          {registrationsInPast3Months}
+        </Typography>
+      </Box>
     </Box>
   );
 }
