bb8534f0ba0a9aad0c13b2991fe54c62e3d155c8
Rudra Patel <patelrudra2003@gmail.com>
2025-03-29 20:01:29 -0400

    feat: Set up diabetes prevention zoom reminder

---
+++ src/app/api/emails/actions/send-zoom-reminder-emails/route.ts
@@ -0,0 +1,77 @@
+import { sendZoomReminderEmail } from "@/server/api/emails/private-mutations";
+import { getUsersByProgram } from "@/server/api/users/private-mutations";
+import {
+  getFailedJsonApiResponse,
+  getSuccessfulJsonApiResponse,
+} from "@/utils/getJsonApiResponse";
+
+export async function POST(request: Request): Promise<Response> {
+  try {
+    const apiKeyHeader = request.headers.get("x-api-key");
+
+    if (!apiKeyHeader || apiKeyHeader !== process.env.API_KEY) {
+      return getFailedJsonApiResponse(401, "Invalid request.");
+    }
+
+    const [healthyHabitsUsers, healthyHabitsError] = await getUsersByProgram(
+      "Healthy Habits For The Long Haul",
+    );
+    const [diabetesPreventionUsers, diabetesPreventionError] =
+      await getUsersByProgram("Diabetes Prevention");
+
+    if (healthyHabitsError !== null || diabetesPreventionError !== null) {
+      return getFailedJsonApiResponse(500, "Internal server error.");
+    }
+
+    const healthyHabitsEmails = healthyHabitsUsers.map((user) => user.email);
+    const diabetesPreventionEmails = diabetesPreventionUsers.map(
+      (user) => user.email,
+    );
+
+    const emailsInBoth = healthyHabitsEmails.filter((email) =>
+      diabetesPreventionEmails.includes(email),
+    );
+
+    const healthyHabitsOnlyEmails = healthyHabitsEmails.filter(
+      (email) => !diabetesPreventionEmails.includes(email),
+    );
+    const diabetesPreventionOnlyEmails = diabetesPreventionEmails.filter(
+      (email) => !healthyHabitsEmails.includes(email),
+    );
+
+    await Promise.allSettled(
+      healthyHabitsOnlyEmails.map((email) =>
+        sendZoomReminderEmail(
+          "Healthy Habits For The Long Haul",
+          "https://google.com",
+          email,
+        ),
+      ),
+    );
+
+    await Promise.allSettled(
+      diabetesPreventionOnlyEmails.map((email) =>
+        sendZoomReminderEmail(
+          "Diabetes Prevention",
+          "https://google.com",
+          email,
+        ),
+      ),
+    );
+
+    await Promise.allSettled(
+      emailsInBoth.map((email) =>
+        sendZoomReminderEmail(
+          "Healthy Habits & Diabetes Prevention",
+          "https://google.com",
+          email,
+        ),
+      ),
+    );
+
+    return getSuccessfulJsonApiResponse(200);
+  } catch (error) {
+    console.error(error);
+    return getFailedJsonApiResponse(500, "Internal server error.");
+  }
+}
