f069c8c322d129d9615b786dddceb3f6260f74c2
Rudra Patel <85089368+RudraPatel2003@users.noreply.github.com>
2025-07-12 22:58:41 -0400

    feat: Add feature flag to ignore automatic jobs (#216)

--- src/utils/constants/featureFlag.ts
+++ src/utils/constants/featureFlag.ts
@@ -3,4 +3,7 @@ export const FEATURE_FLAGS = {
     ENABLED: "isEmailEnabled",
     POSTMARK_SENDING_ENABLED: "isPostmarkSendingEnabled",
   },
+  AUTOMATIC_JOBS: {
+    IS_AUTOMATIC_JOBS_ENABLED: "isAutomaticJobsEnabled",
+  },
 } as const;
